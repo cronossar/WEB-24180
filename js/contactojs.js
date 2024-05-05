1c9127836ad7b974c4b97965714c91f606c34ae5
cronossar <127250505+cronossar@users.noreply.github.com>
2024-05-05 18:37:36 -0300

    domingo

--- js/contactojs.js
+++ js/contactojs.js
@@ -1,9 +1,9 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    var name = document.getElementById('name').value.trim();
-    var email = document.getElementById('email').value.trim();
-    var message = document.getElementById('message').value.trim();
-    var reason = document.getElementById('reason').value.trim();
-    var checkbox = document.getElementById('checkbox').checked;
+    let name = document.getElementById('name').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let message = document.getElementById('message').value.trim();
+    let reason = document.getElementById('reason').value.trim();
+    let checkbox = document.getElementById('checkbox').checked;
   
     if (!name || !email || !message || !reason || !checkbox) {
       event.preventDefault();
